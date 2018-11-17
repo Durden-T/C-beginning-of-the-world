1b5fe530e1b3db36b0168a49abd542eaaf5cf841
Tyler <1251424057@qq.com>
2018-11-17 22:33:15 +0800

    asd

--- a.cpp
+++ a.cpp
@@ -1,30 +1,46 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <vector>
-#include <map>
-#include <set>
-#include <list>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <climits>
-#include <algorithm>
-#include <cmath>
-#include <cctype>
-#include <iterator>
-#include <numeric>
+
 
 using namespace std;
 
-static int __initialSetup = [] {
-    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
-    cin.tie(nullptr);            // untie cin & cout
-    return 0;
-}();
 
 int main()
 {
-    return 0;
+    string a = "张少爷";
+    string ans;
+    for (int i = 0; i < a.size();++i)
+    {
+        int t = a[i];
+        string cur=
+    }
+}
+/**
+	 * 
+	 * 将String转换成unicode编码格式
+	 * @param str
+	 * @return String
+	 * @throws
+	 */
+public
+static String unicodeEncoding(String str)
+{
+    if (str == null || str.trim().length() == 0)
+    {
+        return EMPTY;
+    }
+    StringBuffer unicodeBytes = new StringBuffer();
+    for (int byteIndex = 0; byteIndex < str.length(); byteIndex++)
+    {
+        //字符-Unicode-16进制字符串形式返回
+        String hexB = Integer.toHexString(str.charAt(byteIndex));
+        unicodeBytes.append("\\u");
+        //一定补足4个16进制位--即2个字节
+        if (hexB.length() <= 2)
+        {
+            unicodeBytes.append("00");
+        }
+        unicodeBytes.append(hexB);
+    }
+    return unicodeBytes.toString();
 }
